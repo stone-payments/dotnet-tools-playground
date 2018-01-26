d59b78b06e60a2757b5109041e52d0f142ac4f8f
cnatan <cnatan@stone.com.br>
2018-01-26 15:28:48 -0200

    Add bankaccount project with unit tests.

---
+++ src/BankAccount/Program.cs
@@ -0,0 +1,61 @@
+﻿using Fclp;
+using System;
+
+namespace BankAccountCLI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Read and parse command line arguments.
+            FluentCommandLineParser<ApplicationArguments> parser = new FluentCommandLineParser<ApplicationArguments>();
+
+            parser.Setup(arg => arg.AccountName)
+                .As('a', "account")
+                .Required();
+            parser.Setup(arg => arg.IsCredit)
+                .As('c', "credit");
+            parser.Setup(arg => arg.IsDebit)
+                .As('d', "debit");
+            parser.Setup(arg => arg.Ammount)
+                .As('m', "ammount")
+                .Required();
+            parser.Setup(arg => arg.Balance)
+                .As('b', "balance")
+                .Required();
+
+            parser.Parse(args);
+            ApplicationArguments arguments = parser.Object;
+
+            // Validate exclusive options.
+            if(arguments.IsCredit && arguments.IsDebit)
+            {
+                throw new ArgumentException("Credit and Debit arguments cannot be passed together.");
+            }
+
+            // Run bank account simulation.
+            BankAccount ba = new BankAccount(arguments.AccountName, arguments.Balance);
+
+            if (arguments.IsCredit)
+            {
+                ba.Credit(arguments.Ammount);
+            }
+            else if (arguments.IsDebit)
+            {
+                ba.Debit(arguments.Ammount);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid account operation.");
+            }
+
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+
+            // Wait key for exit in debug mode.
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
