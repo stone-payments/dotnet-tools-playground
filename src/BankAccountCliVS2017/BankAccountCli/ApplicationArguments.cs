2bbdab4585ffbbc23f1fe71baf98ef8b61fe5da9
Carlos Natan <cnatan@stone.com.br>
2018-01-29 11:02:15 -0200

    Added project with new csproj format supported in VS2017 and above.

---
+++ src/BankAccountCliVS2017/BankAccountCli/ApplicationArguments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountCli
+{
+    public class ApplicationArguments
+    {
+        public double Ammount { get; set; }
+        public bool IsCredit { get; set; }
+        public bool IsDebit { get; set; }
+        public string AccountName { get; set; }
+        public double Balance { get; set; }
+    }
+}
